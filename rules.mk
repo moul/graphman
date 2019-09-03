20ea6b22fdd7058ec2aab8efabc12f3110e6c11a
Manfred Touron <m@42.am>
2019-09-03 21:56:21 +0200

    chore: bump deps

--- rules.mk
+++ rules.mk
@@ -23,38 +23,61 @@
 # ||  |       |  |    | |                   /_/_/_/\___/\_,_/_/  |
 # +--------------------------------------------------------------+
 
+all: help
+
 ##
 ## rules.mk
 ##
-ifeq ($(RULESMK),1)
 .PHONY: rulesmk.bumpdeps
 rulesmk.bumpdeps:
 	wget -O rules.mk https://raw.githubusercontent.com/moul/rules.mk/master/rules.mk
-
 BUMPDEPS_STEPS += rulesmk.bumpdeps
-endif
+
+##
+## Maintainer
+##
+
+.PHONY: generate.authors
+generate.authors:
+	echo "# This file lists all individuals having contributed content to the repository." > AUTHORS
+	echo "# For how it is generated, see 'https://github.com/moul/rules.mk'" >> AUTHORS
+	echo >> AUTHORS
+	git log --format='%aN <%aE>' | LC_ALL=C.UTF-8 sort -uf >> AUTHORS
+GENERATE_STEPS += generate.authors
 
 ##
 ## Golang
 ##
 
+ifndef GOPKG
+ifneq ($(wildcard go.mod),)
+GOPKG = $(shell sed '/module/!d;s/^omdule\ //' go.mod)
+endif
+endif
 ifdef GOPKG
 GO ?= go
 
 ifdef GOBINS
 .PHONY: go.install
 go.install:
-	set -e; for dir in $(GOBINS); do ( set -xe; \
+	@set -e; for dir in $(GOBINS); do ( set -xe; \
 	  cd $$dir; \
 	  $(GO) install .; \
 	); done
 INSTALL_STEPS += go.install
+
+.PHONY: go.release
+go.release:
+	goreleaser --snapshot --skip-publish --rm-dist
+	@echo -n "Do you want to release? [y/N] " && read ans && \
+	  if [ $${ans:-N} = y ]; then set -xe; goreleaser --rm-dist; fi
+RELEASE_STEPS += go.release
 endif
 
 .PHONY: go.unittest
 go.unittest:
 	echo "" > /tmp/coverage.txt
-	set -e; for dir in `find . -type f -name "go.mod" | sed 's@/[^/]*$$@@' | sort | uniq`; do ( set -xe; \
+	@set -e; for dir in `find . -type f -name "go.mod" | sed 's@/[^/]*$$@@' | sort | uniq`; do ( set -xe; \
 	  cd $$dir; \
 	  $(GO) test -v -cover -coverprofile=/tmp/profile.out -covermode=atomic -race ./...; \
 	  if [ -f /tmp/profile.out ]; then \
@@ -65,40 +88,33 @@ go.unittest:
 
 .PHONY: go.lint
 go.lint:
-	set -e; for dir in `find . -type f -name "go.mod" | sed 's@/[^/]*$$@@' | sort | uniq`; do ( set -xe; \
+	@set -e; for dir in `find . -type f -name "go.mod" | sed 's@/[^/]*$$@@' | sort | uniq`; do ( set -xe; \
 	  cd $$dir; \
 	  golangci-lint run --verbose ./...; \
 	); done
 
 .PHONY: go.tidy
 go.tidy:
-	set -e; for dir in `find . -type f -name "go.mod" | sed 's@/[^/]*$$@@' | sort | uniq`; do ( set -xe; \
+	@set -e; for dir in `find . -type f -name "go.mod" | sed 's@/[^/]*$$@@' | sort | uniq`; do ( set -xe; \
 	  cd $$dir; \
 	  $(GO)	mod tidy; \
 	); done
 
 .PHONY: go.build
 go.build:
-	set -e; for dir in `find . -type f -name "go.mod" | sed 's@/[^/]*$$@@' | sort | uniq`; do ( set -xe; \
+	@set -e; for dir in `find . -type f -name "go.mod" | sed 's@/[^/]*$$@@' | sort | uniq`; do ( set -xe; \
 	  cd $$dir; \
 	  $(GO)	build ./...; \
 	); done
 
 .PHONY: go.bump-deps
 go.bumpdeps:
-	set -e; for dir in `find . -type f -name "go.mod" | sed 's@/[^/]*$$@@' | sort | uniq`; do ( set -xe; \
+	@set -e; for dir in `find . -type f -name "go.mod" | sed 's@/[^/]*$$@@' | sort | uniq`; do ( set -xe; \
 	  cd $$dir; \
 	  $(GO)	get -u ./...; \
 	); done
 
-.PHONY: go.release
-go.release:
-	goreleaser --snapshot --skip-publish --rm-dist
-	@echo -n "Do you want to release? [y/N] " && read ans && [ $${ans:-N} = y ]
-	goreleaser --rm-dist
-
 BUILD_STEPS += go.build
-RELEASE_STEPS += go.release
 BUMPDEPS_STEPS += go.bumpdeps
 TIDY_STEPS += go.tidy
 LINT_STEPS += go.lint
@@ -106,9 +122,36 @@ UNITTEST_STEPS += go.unittest
 endif
 
 ##
+## Node
+##
+
+ifndef NPM_PACKAGES
+ifneq ($(wildcard package.json),)
+NPM_PACKAGES = .
+endif
+endif
+ifdef NPM_PACKAGES
+.PHONY: npm.publish
+npm.publish:
+	@echo -n "Do you want to npm publish? [y/N] " && read ans && \
+	@if [ $${ans:-N} = y ]; then \
+	  set -e; for dir in $(NPM_PACKAGES); do ( set -xe; \
+	    cd $$dir; \
+	    npm publish --access=public; \
+	  ); done; \
+	fi
+RELEASE_STEPS += npm.publish
+endif
+
+##
 ## Docker
 ##
 
+ifndef DOCKER_IMAGE
+ifneq ($(wildcard Dockerfile),)
+DOCKER_IMAGE = $(notdir $(PWD))
+endif
+endif
 ifdef DOCKER_IMAGE
 .PHONY: docker.build
 docker.build:
@@ -128,9 +171,7 @@ endif
 TEST_STEPS += $(UNITTEST_STEPS)
 TEST_STEPS += $(LINT_STEPS)
 TEST_STEPS += $(TIDY_STEPS)
-ALL_STEPS += $(TEST_STEPS)
 
-all: $(ALL_STEPS)
 
 ifneq ($(strip $(TEST_STEPS)),)
 .PHONY: test
@@ -162,7 +203,31 @@ ifdef BUILD_STEPS
 build: $(BUILD_STEPS)
 endif
 
+ifdef RELEASE_STEPS
+.PHONY: release
+release: $(RELEASE_STEPS)
+endif
+
 ifdef BUMPDEPS_STEPS
 .PHONY: bumpdeps
 bumpdeps: $(BUMPDEPS_STEPS)
 endif
+
+ifdef GENERATE_STEPS
+.PHONY: generate
+generate: $(GENERATE_STEPS)
+endif
+
+.PHONY: help
+help:
+	@echo "General commands:"
+	@[ "$(BUILD_STEPS)" != "" ]     && echo "  build"     || true
+	@[ "$(BUMPDEPS_STEPS)" != "" ]  && echo "  bumpdeps"  || true
+	@[ "$(GENERATE_STEPS)" != "" ]  && echo "  generate"  || true
+	@[ "$(INSTALL_STEPS)" != "" ]   && echo "  install"   || true
+	@[ "$(LINT_STEPS)" != "" ]      && echo "  lint"      || true
+	@[ "$(RELEASE_STEPS)" != "" ]   && echo "  release"   || true
+	@[ "$(TEST_STEPS)" != "" ]      && echo "  test"      || true
+	@[ "$(TIDY_STEPS)" != "" ]      && echo "  tidy"      || true
+	@[ "$(UNITTEST_STEPS)" != "" ]  && echo "  unittest"  || true
+	@# FIXME: list other commands
